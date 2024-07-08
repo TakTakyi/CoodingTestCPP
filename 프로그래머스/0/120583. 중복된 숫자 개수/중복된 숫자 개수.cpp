ce6917fcad9915fb4d1b55ede96d52f66f812fd4
김정택 <110222772+TakTakyi@users.noreply.github.com>
2024-07-08 15:03:44 +0900

    Update 중복된 숫자 개수.cpp

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.cpp
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 int solution(vector<int> array, int n) {
-    int answer = 0;
+    int answer = 0; //결과값 변수
     
-    for (int ii = 0; ii < array.size(); ii++)
+    for (int ii = 0; ii < array.size(); ii++) //array의 크기 만큼 반복
     {
-        if (array[ii] == n)
+        if (array[ii] == n) //array배열 요소중 매개변수 n과 같으면 true
         {
-            answer++;
+            answer++; //결과값에 1을 더한다
         }
     }
-    return answer;
+    return answer; //결과값을 리턴한다
 }
