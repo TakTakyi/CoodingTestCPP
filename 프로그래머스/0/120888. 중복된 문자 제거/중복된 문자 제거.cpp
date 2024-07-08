f3e7aead7e07f074b4193309440c0f52ade45d51
김정택 <110222772+TakTakyi@users.noreply.github.com>
2024-07-08 14:36:13 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <map>
+
+using namespace std;
+
+int arr[28];
+map<char, int> mp;
+
+string solution(string my_string) {
+    string answer = "";
+    
+    for(int i=0; i<my_string.size(); i++){
+        if(mp[my_string[i]]==0)
+            mp[my_string[i]]=1, answer+=my_string[i];
+    }
+    
+    return answer;
+}
