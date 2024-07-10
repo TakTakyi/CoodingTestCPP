f51a805a400fe8f1f2c14afde7433545b72c96f1
김정택 <110222772+TakTakyi@users.noreply.github.com>
2024-07-10 15:40:23 +0900

    [level 0] Title: 소인수분해, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.cpp
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.cpp
@@ -6,27 +6,35 @@ using namespace std;
 vector<int> solution(int n) {
     vector<int> answer;
     
-    int cnt=2;
+    int cnt=2;  //소인수분해를 하기 위한 소수, 제일 작은 값인 2부터
     
-    while(1){
-        bool check=false;
-        while(1){
-            if(n%cnt==0){
-                n/=cnt;
-                check=true;
+    //이중 반복문
+    //무한반복 사용 while(1)
+    while(1)
+    {
+        bool check=false; 
+        //한번이라도 나누어진적이 있는지 소수로 나누었졌는지 확인하는 변수
+        
+        while(1)
+        {
+            if(n%cnt==0) // 소인수 분해 가능
+            {
+                n/=cnt; //나누고 몪을 변수에 대입
+                check=true; // 한번이라도 나누어졌기에 true
             }
                 
-            else if(n%cnt!=0 && check){
-                answer.push_back(cnt);
-                break;
+            else if(n%cnt!=0 && check) //cnt로 더이상 나누어지지 않을때
+            {
+                answer.push_back(cnt); //결과값 입력
+                break;//반복종료
             }
             
-            else if(n%cnt!=0 && !check) break;
+            else if(n%cnt!=0 && !check) break; //소수와 2와 3의 배수를 제외 하기위한 조건문 반복종료
         }
         
-        if(n==1) break;
+        if(n==1) break; //n이 1이 되면 소인수분해가 완료된것이기에 반복 탈출
         
-        cnt++;
+        cnt++; // +1 자기자신으로만 나누어지는 수를 찾기 위해(소수)/2345순서로 나누기 위해
     }
     
     return answer;
