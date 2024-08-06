f871f4ce04b464aa76bf6b27993d575381eddd06
김정택 <110222772+TakTakyi@users.noreply.github.com>
2024-08-06 13:47:20 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.cpp
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.cpp
@@ -6,11 +6,14 @@ using namespace std;
 
 vector<int> solution(vector<string> keyinput, vector<int> board) {
     vector<int> answer;
-    int x = 0, y = 0;
-    int b_x = board[0]/2, b_y = board[1]/2;
+    int x = 0, y = 0; // 좌표 계산을 위한 정수형 변수
+    //board에 경우 0,0을 기준으로 +와 -가 있기에 반으로 나눈다
+    int b_x = board[0]/2, b_y = board[1]/2; // board의 크기를 저장한 변수 
     
+    //매개변수로 들어온 벡터의 사이즈만큼 반복
     for (int ii = 0; ii < keyinput.size(); ii++)
     {
+        //문자열을 통해 이동할 좌표와 board의 사이즈를 벗어나는지 체크하여 좌표값을 이동한다
         if(keyinput[ii] == "left" && x > -b_x)
         {
             x -= 1;
@@ -29,7 +32,8 @@ vector<int> solution(vector<string> keyinput, vector<int> board) {
         }
     }
     
-    cout << x << y <<endl;
+    //cout << x << y <<endl;
+    //반복이 완료되면 x좌표부터 answer에 좌표값을 push_back 한다
     answer.push_back(x);
     answer.push_back(y);
     
