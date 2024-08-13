b39a10ac2ca8bd3ef430d508102916d5cb7f12b0
김정택 <110222772+TakTakyi@users.noreply.github.com>
2024-08-13 17:01:14 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<vector<int>> score) {
+    vector<int> answer;
+    vector<int> m_AVG;
+    
+    //성적의 평균값 구하기
+    //평균값을 m_AVG에 삽입
+    for (int ii = 0; ii < score.size(); ii++)
+    {
+        int avg = (score[ii][0] + score[ii][1]);
+        m_AVG.push_back(avg);
+    }
+    
+    //평균값을 벡터 사이즈 만큼 반복하면서 비교
+    //m_AVG[ii] 값보다 큰값이 있을경우 rank를 증가시킨다
+    //안쪽 반복문이 끝나면 rank값을 결과값에 삽입한다
+    for (int ii = 0; ii < m_AVG.size(); ii++)
+    {
+        int rank = 1;
+        for (int jj = 0; jj < m_AVG.size(); jj++)
+        {
+            if (m_AVG[ii] < m_AVG[jj])
+            {
+                rank++;
+            }
+        }
+        
+        answer.push_back(rank);
+    }
+    
+    return answer;
+}
